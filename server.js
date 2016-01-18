88d4a2a1fddaa7defbe44ade7dbd9bae91338005
Dale Anderson <danderson00@gmail.com>
2016-01-18 15:25:57 -0800

    Added loaderio

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var app = require('express')(),
-    mobileApp = require('azure-mobile-apps')()
+    mobileApp = require('azure-mobile-apps')({ homePage: true, skipVersionCheck: true })
+
+app.get('/loaderio-2224bb5afed152e4736909c3531507f4/', function (req, res, next) {
+    res.send('loaderio-2224bb5afed152e4736909c3531507f4')
+})
 
 mobileApp.tables.add('crud')
 app.use(mobileApp)
